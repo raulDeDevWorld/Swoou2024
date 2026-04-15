c7d5466c844934144a07dd6c48f4df101309bbe2
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2026-04-15 19:49:25 -0400

    Fix indentation in Card component
    DC

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import Button from '../components/Button'
 import style from '../styles/Card.module.css'
 
 export default function Card (props) {
-    return (
+    return (    
 
         <div
             className="w-full flex flex-col justify-between border-[1px] border-gray-200 shadow-2xl my-[25px] p-[15px] text-black"
